f923c315012542230c1c2e81cf4c26831859a6a5
Ivan <verba447@gmail.com>
2026-07-18 11:59:15 +0200

    Init Spring Boot

---
+++ src/main/java/verba/employee_data_service/EmployeeDataServiceApplication.java
@@ -0,0 +1,13 @@
+package verba.employee_data_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/verba/employee_data_service/EmployeeDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package verba.employee_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
